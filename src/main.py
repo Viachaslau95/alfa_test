60ba408532ce591c556c671ee78d5828d4f7d352
V.Kulchynski <6891094@mail.ru>
2023-08-01 16:19:04 +0300

    connect with celery

--- src/main.py
+++ src/main.py
@@ -1,19 +1,51 @@
+from datetime import datetime, timedelta
 from io import BytesIO
 
 import openpyxl
 import pandas as pd
+from celery import Celery
 from fastapi import FastAPI, Depends, UploadFile, HTTPException
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.schemas import Client
-from src.database import get_async_session
-from src.models import client, message
+from config import REDIS_HOST, REDIS_PORT
+from schemas import Client
+from database import get_async_session
+from models import client, message
+# from tasks import generate_table
 
 app = FastAPI(
     title='Alfa test'
 )
+celery_app = Celery(
+    'tasks',
+    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}"
+)
 
+
+@celery_app.task
+def generate_table():
+    now = datetime.now()
+    if now.hour >= 22 or now.hour < 9:
+        return
+
+    with get_async_session() as session:
+        table_creation(session)
+
+
+# Delayed start of a task every hour during the week
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        timedelta(hours=1),
+        generate_table.s(),
+        expires=timedelta(weeks=1)
+    )
+
+@app.get("/run-task")
+def run_task():
+    generate_table.delay()
+    return {"message": "Task is scheduled"}
 
 @app.get("/clients")
 async def get_clients(session: AsyncSession = Depends(get_async_session)):
@@ -31,26 +63,43 @@ async def get_messages(session: AsyncSession = Depends(get_async_session)):
     return result
 
 
-@app.get("/gen_table")
-async def table_creation(session: AsyncSession = Depends(get_async_session)):
-    query_client = select(client).with_only_columns(client.c.phone)
-    clients = await session.execute(query_client)
-    data_clients = clients.fetchall()
+async def table_creation(session: AsyncSession = None):
+    if session is None:
+        async with get_async_session() as session:
+            print(2)
+            query_client = select(client).with_only_columns(client.c.phone)
+            clients = await session.execute(query_client)
+            data_clients = clients.fetchall()
 
-    query_message = select(message).order_by(message.c.id.desc()).with_only_columns(message.c.text)
-    messages = await session.execute(query_message)
-    data_messages = messages.fetchall()
+            query_message = select(message).order_by(message.c.id.desc()).with_only_columns(message.c.text)
+            messages = await session.execute(query_message)
+            data_messages = messages.fetchall()
 
-    # print(random.SystemRandom().choice(list(messages)))
-    # idx = [r[0] for r in list(clients)]
+            df1 = pd.DataFrame(data_clients)
+            df2 = pd.DataFrame(list(data_messages[0]), columns=messages.keys())
 
-    df1 = pd.DataFrame(data_clients)
-    df2 = pd.DataFrame(list(data_messages[0]), columns=messages.keys())
+            combined_df = pd.concat([df1, df2], axis=1)
+            output_file = 'client_message_data.xlsx'
+            combined_df.to_excel(output_file, index=False)
 
-    combined_df = pd.concat([df1, df2], axis=1)
-    output_file = 'client_message_data.xlsx'
-    combined_df.to_excel(output_file, index=False)
+        return output_file
+    else:
 
+        query_client = select(client).with_only_columns(client.c.phone)
+        clients = await session.execute(query_client)
+        data_clients = clients.fetchall()
+
+        query_message = select(message).order_by(message.c.id.desc()).with_only_columns(message.c.text)
+        messages = await session.execute(query_message)
+        data_messages = messages.fetchall()
+
+        df1 = pd.DataFrame(data_clients)
+        df2 = pd.DataFrame(list(data_messages[0]), columns=messages.keys())
+
+        combined_df = pd.concat([df1, df2], axis=1)
+        output_file = 'client_message_data.xlsx'
+        combined_df.to_excel(output_file, index=False)
+        return output_file
 
 @app.post("/uploadFile/")
 async def create_upload_file(file: UploadFile):
